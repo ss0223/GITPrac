0d2f3cabdbe5d3acd3d5ee386c84a01d61d5e132
Suresh <tnscsuresh@gmail.com>
2024-07-29 23:40:56 -0700

    sasti

--- Hellojava.java
+++ Hellojava.java
@@ -5,8 +5,9 @@ public class Hellojava {
         JFrame frame = new JFrame("Hello, Java!");
         JLabel label = new JLabel("Hello, Java!", JLabel.CENTER);
         frame.add(label);
-        frame.setSize(550,550);
+        frame.setSize(500,500);
         frame.setVisible(true);
         //System.out.println("Hello Java");
+
     }
 }
